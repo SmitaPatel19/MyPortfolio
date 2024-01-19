e0f4556c552ef76854c96e9c1ff7816920573a29
SmitaPatel19 <miss.smitapatel04@gmail.com>
2024-01-19 13:28:18 +0530

    made for web

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Smita Patel',
+      title: 'My Portfolio',
       debugShowCheckedModeBanner: false,
       home: Home(),
     );
